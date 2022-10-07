1c73e54c0023f7683731364e880cf34da2d1335d
Trevor F. Freeman <trevor.freeman@utexas.edu>
2022-10-07 14:57:07 -0400

    Refactor CLI to its own module

---
+++ tests/test_cli.py
@@ -0,0 +1,9 @@
+from click.testing import CliRunner
+from cfb_rankings_analysis.cli import cli
+
+
+def test_say_hello():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["say-hello"])
+    assert result.exit_code == 0
+    assert "Hello World" in result.output
