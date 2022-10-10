47d96b3aa6b60716d1068d5e0d1375850055c17b
Trevor F. Freeman <trevor.freeman@utexas.edu>
2022-10-10 16:41:31 -0400

    Refactor configuration test

--- tests/test_data/test_download_data.py
+++ tests/test_data/test_download_data.py
@@ -7,7 +7,7 @@ import cfbd
 def test_configure_cfbd_api_auth():
     configuration = configure_cfbd_api_auth()
 
-    assert type(configuration) == cfbd.configuration.Configuration
+    assert isinstance(configuration, cfbd.configuration.Configuration)
     assert configuration.api_key_prefix.get("Authorization") == "Bearer"
     assert isinstance(configuration.api_key.get("Authorization"), str)
 
