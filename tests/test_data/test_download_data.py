8da8c2d649e33244b5db76839290bbef5150e9d0
Trevor F. Freeman <trevor.freeman@utexas.edu>
2022-10-10 17:43:21 -0400

    Add download Elo ratings

--- tests/test_data/test_download_data.py
+++ tests/test_data/test_download_data.py
@@ -1,7 +1,19 @@
 from cfb_rankings_analysis.data.download_data import (
-    configure_cfbd_api_auth, get_environment_variable
+    configure_cfbd_api_auth, download_elo, get_environment_variable
 )
 import cfbd
+
+
+def test_download_elo():
+    # test download Elo for single team
+    expected_elo = cfbd.models.team_elo_rating.TeamEloRating(conference="SEC", elo=1756.0, team="Tennessee", year=2022)
+    actual_elo = download_elo(year=2022, team="Tennessee", week=1)[0]
+    assert actual_elo == expected_elo
+
+    # test download Elo for all teams from one week
+    expected_number_elos = 131
+    actual_number_elos = len(download_elo(year=2022, week=1))
+    assert actual_number_elos == expected_number_elos
 
 
 def test_configure_cfbd_api_auth():
